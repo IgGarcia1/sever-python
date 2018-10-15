1fecd50e1cf23c213835c24884d47903b3d79c06
igor <igorggarcia1@gmail.com>
2018-10-14 21:38:52 -0300

    v2

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
 
-server = Flask(__name__)
+app = Flask(__name__)
 
 
-@server.route("/")
+@app.route("/")
 def ola():
     return "<h1> Olá Servidor python </h1>"
 
 
 if __name__ == "__main__":
-    server.run()
+    app.run()
